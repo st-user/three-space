ec413a1a650e9655b5dd7b1b620bbd5847f59fb4
st-user <nck.cd.339@gmail.com>
2021-06-10 16:08:29 +0900

    Update version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /*! For license information please see https://three-space.ajizablg.com/oss-licenses.json */
 
 import main from './main.js';
-window.APP_VERSION = '1.0.0';
+window.APP_VERSION = '1.0.1';
 
 main();
